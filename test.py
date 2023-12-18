6324eea504190e1cf054594266f32ddc491feb5d
alejandro-escorihuela <alescori@uji.es>
2023-12-18 20:11:15 +0100

    change h_ini

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     tmp0 = tm.time()
     x0 = [-2*np.sqrt(2/3), np.sqrt(2/3), -np.sqrt(2/3)]
     b0 = [1.0, 0.0, 0.0]
-    x1, info = arcpy(f, g, x0, b0, 0.0, 100.0, action = 2, method = "hybr")
+    x1, info = arcpy(f, g, x0, b0, 0.0, 10.0, action = 0, method = "hybr")
     print("t =", tm.time() - tmp0)
     print(x1)
     print(info)
